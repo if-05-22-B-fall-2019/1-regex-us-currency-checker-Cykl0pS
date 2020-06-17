a1d04a14b8d3b88cf8104744f192de4eb52552ce
Kemal Yavuz <35776056+Cykl0pS@users.noreply.github.com>
2020-06-17 22:34:34 +0200

    Currency Checker by Kemal Yavuz

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        String regex = "\\$\\s?\\d+(\\.\\d{1,2})?";
+        Pattern pattern = Pattern.compile(pattern);
+        return pattern.matcher(usCurrencyString);
     }
 }
